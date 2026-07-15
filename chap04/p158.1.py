bc366f6b5515771088b473a7e75d09502b3fd889
itc-n26005 <n26005@std.it-college.ac.jp>
2026-07-15 02:58:22 +0000

    動作確認

---
+++ chap04/p158.1.py
@@ -0,0 +1,8 @@
+def show_args(*args):
+    '''与えられた複数の位置引数をタプルにまとめて受け取りそのタプルを表示して返す'''
+    print('Positional arguments:', args)
+    return args
+
+positional_args = (4, 5, 6,  'ya')
+#positional_args = [4, 5, 6,  'ya']
+print(show_args(*positional_args))
---
+++ chap04/p158.2.py
@@ -0,0 +1,7 @@
+def show_kwargs(**kwargs):
+    '''与えられた複数のキーワード引数を辞書にまとめて受け取りその辞書を表示して返す'''
+    print('Keyword arguments:', kwargs)
+    return kwargs
+
+keyword_args = {'pasta':'ペンネ', 'drink':'赤ワイン', 'main_dish':'肉料理', 'n_customers':3}
+print(show_kwargs(**keyword_args))
